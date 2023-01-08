00dc1194eb02e3757262354399ec0fbda646460f
Zaykov <zk8888m@gmail.com>
2023-01-08 17:00:03 +0300

    Сделано задание 3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,13 @@
-function compareArrays(arr1, arr2) {
-  
-}
+ const compareArrays = (arr1, arr2) => arr1.every((el, i) => el === arr2[i]) && arr1.length === arr2.length;
+
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let result = users.filter(users => users.gender === gender).map(users => users.age).reduce((acc, item, index, arr) => {
+    acc += item;
+    if (index === arr.length -1) {
+    return acc / arr.length;
+    }
+    return acc;
+  }, 0)
+  return result;
 }
